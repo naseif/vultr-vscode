0dcb73e737e8fceebedfaf47beb58283bc9be6a5
naseif <d2mds21@gmail.com>
2021-10-15 12:36:45 +0000

    fixed stop function

--- src/stopInstance.js
+++ src/stopInstance.js
@@ -3,23 +3,23 @@ const { logger } = require("./logger");
 const { Initialize } = require("./vultrInitializer");
 /**
  * Destroys the created Instance
- * @param {VULTR CLIENT} init
- * @param {INSTANCE ID} id
  * @returns void
  */
 module.exports.stopInstance = async () => {
   const key = prompt(`Vultr Key: `);
   const instanceID = prompt(`Instance ID: `);
   const vultr = Initialize(key);
-  const getInstance = await vultr.instances.getInstance(instanceID);
-  if (!getInstance || getInstance.instances.length === 0)
+  const getInstance = await vultr.instances.getInstance({
+    "instance-id": instanceID,
+  });
+  if (!getInstance)
     return logger(
       `No instance found with this ID or maybe it does not exist!`,
       `error`
     );
   try {
     await vultr.instances.deleteInstance({
-      "instance-id": instanceID,
+      "instance-id": `${instanceID}`,
     });
   } catch (err) {
     logger(`Error: ${err.message}`, `error`);
